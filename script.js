65f7649fd4605153b3f1b5cf370ee7e580b7c837
lavell119 <leewhiz119@gmail.com>
2022-04-29 10:37:43 -0400

    added labels to options

---
+++ script.js
@@ -0,0 +1,4 @@
+let nextButton=document.querySelector("next-button")
+let buySelector=document.querySelector("buy-selector")
+
+nextButton.onClick=(buySelector)=>{buySelector.classList.add("hidden")}

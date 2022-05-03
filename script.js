c3bdc5cea70feed5a715fbfa65e6d2478e3ba63a
lavell119 <leewhiz119@gmail.com>
2022-05-03 01:04:08 -0400

    added pages

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const buySelector=document.getElementById('buy-selector')
 const btcOption=document.querySelector('.btc-option')
 const ethOption=document.querySelector('.eth-option')
 const selector=document.querySelector('.selector')
+const priceOption=document.querySelectorAll('.price-option')
+const priceOptions=document.querySelector('.price-options')
+const confirmation=document.querySelector('.confirmation')
+const dollarAmount=document.querySelector('.dollar-amount')
+const subTitle=document.querySelector('.sub-title')
 
 
+btcOption.addEventListener('click', ()=> {
+buySelector.classList.add('hidden')
+priceOptions.classList.add('show')
+subTitle.innerText='HOW MUCH WOULD YOU LIKE TO BUY?'
+})
+ethOption.addEventListener('click', ()=> {buySelector.classList.add('hidden')
+priceOptions.classList.add('show')
+subTitle.innerText='HOW MUCH WOULD YOU LIKE TO BUY?'})
+priceOption.forEach(option=>option.addEventListener('click', ()=> {priceOptions.classList.remove('show')
+    confirmation.classList.add('show')
+    let innerText=option.innerText
+    dollarAmount.innerText=innerText
+    subTitle.innerText='CONFIRMATION'
 
-btcOption.addEventListener('click', ()=> {buySelector.classList.add('hidden')})
-ethOption.addEventListener('click', ()=> {buySelector.classList.add('hidden')})
+}))
 
 
 
